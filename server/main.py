fb09f0c33b6b7b9c5d78e0a03cc1ddd9eb038594
Furkan Poyraz <furkanpoyraz@users.noreply.github.com>
2022-08-23 23:09:53 +0200

    ability to add and remove channels

--- server/main.py
+++ server/main.py
@@ -2,12 +2,43 @@
 
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+import json
 
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/updateShutters', methods = ['PATCH'])
-def shutters():
+@app.route('/channels', methods = ['GET'])
+def getChannels():
+    file = open("channels.json", "r")
+    data = json.load(file)
+    file.close()
+
+    return data
+
+@app.route('/channels', methods = ['DELETE'])
+def deleteChannel():
+    channelID = request.json['channelID']
+
+    file = open("channels.json", "r")
+    data = json.load(file)
+    file.close()
+
+    channels = data['channels']
+
+    for i, channel in enumerate(channels):
+        if channel['id'] == channelID:
+            channels.pop(i)
+
+    file = open("channels.json", "w+")
+    file.write(json.dumps(data))
+    file.close()
+
+    response = jsonify({"result": "channel removed successfully"})
+
+    return response
+
+@app.route('/shutters', methods = ['PATCH'])
+def setShutters():
     channel = request.json['channel']
     action = request.json['action']
 
@@ -16,7 +47,7 @@ def shutters():
     else:
         result = "Button pressed! Channel: " + str(channel) + ' Action: ' + action
 
-    response = jsonify({'result': result})
+    response = jsonify({"result": result})
     return response
 
 app.run(debug=True)
--- src/ChannelList.js
+++ src/ChannelList.js
@@ -1,16 +1,37 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function ChannelList() {
   const [error, setError] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
+  const [channels, setChannels] = useState([]);
+  const [channelsTotal, setChannelsTotal] = useState(5);
 
-  const channels = 5;
   const actions = ['UP', 'MY', 'DOWN'];
 
-  const updateShutters = (channel, action) => {
+  const apiUrl = (process.env.NODE_ENV !== 'production') ? "http://localhost:5000" : "/api";
+
+  const fetchChannels = () => {
+    fetch(apiUrl + "/channels", {
+      method: "GET",
+      headers: { "Content-type": "application/json" },
+    })
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          setChannels(result.channels)
+          setChannelsTotal(result.channelsTotal)
+        },
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      )
+  }
+
+  const setShutters = (channel, action) => {
     return () => {
-      alert('test ' + channel + ' / ' + action);
-      fetch("http://localhost:5000/updateShutters", {
+      fetch(apiUrl + "/shutters", {
         method: "PATCH",
         headers: { "Content-type": "application/json" },
         body: JSON.stringify({ channel: channel, action: action })
@@ -18,31 +39,84 @@ function ChannelList() {
         .then(res => res.json())
         .then(
           (result) => {
-            setIsLoaded(true);
             console.log(result);
           },
           (error) => {
-            setIsLoaded(true);
             setError(error);
           }
         )
     }
   }
 
-  return (
-    <ul className="channel-list">
-      {[...Array(channels).keys()].map((i) =>
-        <li className="channel-item" key={i.toString()}>
-          <div className="channel-label">Channel {i+1}</div>
-          <div className="channelAction-list">
-            {actions.map((action) =>
-              <button key={action} type="button" className="channelAction-item" onClick={updateShutters(i+1, action)}>{action}</button>
-            )}
-          </div>
+  const addChannel = () => {
+    fetch(apiUrl + "/channels", {
+      method: "PUT",
+      headers: { "Content-type": "application/json" },
+    })
+      .then(res => res.json())
+      .then(
+        (result) => {
+          console.log(result);
+        },
+        (error) => {
+          setError(error);
+        }
+      )
+  }
+
+  const removeChannel = (channelID) => {
+    return () => {
+      fetch(apiUrl + "/channels", {
+        method: "DELETE",
+        headers: { "Content-type": "application/json" },
+        body: JSON.stringify({ channelID: channelID })
+      })
+        .then(res => res.json())
+        .then(
+          (result) => {
+            const updatedChannels = channels.filter((channel) => { return channel.id !== channelID });
+            setChannels(updatedChannels);
+            localStorage.setItem('channelData', JSON.stringify({ channelsTotal: channels, channels: channelsTotal }));
+            console.log(result);
+          },
+          (error) => {
+            setError(error);
+          }
+        )
+    }
+  }
+
+  useEffect(() => {
+    fetchChannels()
+  }, []);
+
+  if (isLoaded) {
+    return (
+      <ul className="channel-list">
+        {channels.map((channel) =>
+          <li className="channel-item" key={channel.id}>
+            <button type="button" className="channel-removeBtn" onClick={removeChannel(channel.id)}>&times;</button>
+            <div className="channel-label">
+              {channel.name}
+              <span className="channel-labelID">Channel {channel.id}</span>
+            </div>
+            <div className="channelAction-list">
+              {actions.map((action) =>
+                <button key={action} type="button" className="channelAction-item" onClick={setShutters(channel.id, action)}>{action}</button>
+              )}
+            </div>
+          </li>
+        )}
+        {(channels.length !== channelsTotal) &&
+        <li className="channel-item channel-item--add">
+          <button type="button" className="addChannel" onClick={addChannel}>+ Add Channel</button>
         </li>
-      )}
-    </ul>
-  );
+        }
+      </ul>
+    );
+  } else {
+    return (<span className="loader"></span>)
+  }
 }
 
 export default ChannelList;
